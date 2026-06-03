6c5c23be591bfdd2ce56144aecbbacca94c06b10
Your Name <dharmadudu@gmail.com>
2026-06-03 19:05:34 +0530

    ProductCard Image ui change

--- client/src/components/product/ProductCard.jsx
+++ client/src/components/product/ProductCard.jsx
@@ -3,36 +3,27 @@ import { FaEdit, FaTrash, FaUpload } from "react-icons/fa";
 const ProductCard = ({ product, onEdit, onDelete, onPublish }) => {
   console.log(product);
 
- 
-  const imageSrc =
-  product.images?.length > 0
-    ? product.images[0]
-    : null;
+  const imageSrc = product.images?.length > 0 ? product.images[0] : null;
 
   return (
     <div className="border rounded-xl shadow-sm bg-white p-4 flex flex-col h-full">
+     <div className="w-full h-50 bg-gray-100 rounded-lg mb-4 flex items-center justify-center overflow-hidden">
+  {imageSrc ? (
+    <img
+      src={imageSrc}
+      alt={product.name}
+      className="w-full h-full object-contain"
+    />
+  ) : (
+    <span className="text-gray-400 text-sm">No Image</span>
+  )}
+</div>
 
-    
-      <div className="w-full h-40 bg-gray-100 rounded-lg mb-4 flex items-center justify-center overflow-hidden">
-        {imageSrc ? (
-         <img
-  src={imageSrc}
-  alt={product.name}
-  className="w-full h-full object-cover"
-/>
-        ) : (
-          <span className="text-gray-400 text-sm">No Image</span>
-        )}
-      </div>
-
-  
       <h2 className="font-semibold text-lg mb-2 text-gray-800 break-words">
         {product.name}
       </h2>
 
-  
       <div className="text-sm text-gray-600 space-y-1 flex-1">
-
         <p className="flex justify-between">
           <span className="font-medium">Type:</span>
           <span className="font-semibold">{product.type}</span>
@@ -67,26 +58,22 @@ const ProductCard = ({ product, onEdit, onDelete, onPublish }) => {
 
         <p className="flex justify-between">
           <span className="font-medium">Exchange:</span>
-          <span className="font-semibold">
-            {product.returnPolicy || "No"}
-          </span>
+          <span className="font-semibold">{product.returnPolicy || "No"}</span>
         </p>
-
       </div>
 
       <div className="flex justify-between gap-2 mt-4">
-
-       <button
-  onClick={() => onPublish(product._id)}
-  className={`flex items-center gap-1 font-semibold px-4 py-2 text-sm text-white rounded-lg transition-all ${
-    product.isPublished
-      ? "bg-gradient-to-r from-[#52D407] to-[#37C100]"
-      : "bg-gradient-to-r from-[#000FB4] to-[#4050FF]"
-  }`}
->
-  <FaUpload />
-  {product.isPublished ? "Unpublish" : "Publish"}
-</button>
+        <button
+          onClick={() => onPublish(product._id)}
+          className={`flex items-center gap-1 font-semibold px-4 py-2 text-sm text-white rounded-lg transition-all ${
+            product.isPublished
+              ? "bg-gradient-to-r from-[#52D407] to-[#37C100]"
+              : "bg-gradient-to-r from-[#000FB4] to-[#4050FF]"
+          }`}
+        >
+          <FaUpload />
+          {product.isPublished ? "Unpublish" : "Publish"}
+        </button>
 
         <button
           onClick={() => onEdit?.(product)}
@@ -99,11 +86,9 @@ const ProductCard = ({ product, onEdit, onDelete, onPublish }) => {
           onClick={() => onDelete?.(product._id)}
           className="flex items-center font-semibold gap-1 px-3 py-2 text-sm border border-[#cccdd1]  text-gray-400 rounded-lg hover:opacity-90 transition"
         >
-          <FaTrash /> 
+          <FaTrash />
         </button>
-
       </div>
-
     </div>
   );
 };
