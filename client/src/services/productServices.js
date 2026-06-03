6f5a2f06f554c924c01ee052aa801cfaf7326980
Your Name <dharmadudu@gmail.com>
2026-06-03 12:33:21 +0530

    Fix production API URL

--- client/src/services/productServices.js
+++ client/src/services/productServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/products";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getProducts = () => {
   return axios.get(API_URL);
