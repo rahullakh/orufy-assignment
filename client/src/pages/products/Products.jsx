e36520ed7899d8b27b566ce51b9b9d95832bcd99
Your Name <dharmadudu@gmail.com>
2026-06-06 17:36:27 +0530

    List Set

--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -117,7 +117,7 @@ const Products = () => {
   };
 
   return (
-    <div className="min-h-screen p-4 sm:p-10">
+    <div className="min-h-screen py-4">
 
       {!loading && products.length === 0 ? (
         <div className="flex items-center justify-center min-h-[70vh]">
